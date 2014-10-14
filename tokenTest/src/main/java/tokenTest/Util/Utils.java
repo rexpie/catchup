dd426e5333faafb89f8d4fd703e1b0108c123913
rexpie <rexpie@gmail.com>
2014-10-14 22:20:19 +0800

    add age util;

---
+++ tokenTest/src/main/java/tokenTest/Util/Utils.java
@@ -0,0 +1,20 @@
+package tokenTest.Util;
+
+import java.util.Date;
+
+import org.joda.time.LocalDate;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+
+public class Utils {
+	public static Integer getAge(Date birthday){
+		if (birthday == null) return null;
+		
+		LocalDate birthdate = new LocalDate (birthday.getTime());          //Birth date
+		LocalDate now = new LocalDate();                    //Today's date
+		Period period = new Period(birthdate, now, PeriodType.yearMonthDay());
+		
+		return period.getYears();
+		
+	}
+}
